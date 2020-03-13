49b9fb2b56795075326390ad2f36eb4adf2dd2bd
Tamas Erdos <github@tamaserdos.com>
2020-03-13 23:47:02 +0100

    coloring score

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="w-full max-w-sm mx-auto">
       <h1 className="mt-4 font-bold text-gray-900 text-4xl text-center">Ulti Elszámoló</h1>
-        <form className="bg-white shadow-2xl rounded mx-2 px-1 pt-2 pb-2 mb-4">
+        <form className="bg-gray-100 shadow-2xl rounded mx-2 px-1 pt-2 pb-2 mb-4 border border-gray-300">
           {scores.map((raw, i) => <PlayerSection
             i={i}
             raw={raw}
@@ -44,19 +44,25 @@ function App() {
 }
 
 function PlayerSection({i, raw, score, updateScore}) {
+  let scoreColor = "text-black"
+  if (score > 0) {
+    scoreColor = "text-green-700"
+  } else if (score < 0) {
+    scoreColor = "text-red-700"
+  }
   return (
     <div className="mt-4">
-    <div className="grid grid-cols-3 gap-4 mb-2 items-baseline">
-    <label className="block text-gray-600 text-sm font-bold mb-2 text-lg" >
+    <div className="grid grid-cols-3 gap-4 mb-2 items-middle items-baseline">
+    <label className="block text-gray-600 text-sm font-bold mb-2 text-xl" >
       {`Játékos ${i+1}`}
       </label>
-        <input className="bg-gray-200 text-gray-700 rounded-full border-none text-right text-xl pr-3"
+        <input className="bg-gray-200 text-gray-700 rounded-full border-none text-right text-xl pr-3 self-start"
           value={raw}
           onChange={function(e) {updateScore(i, e.target.value)}}
           autoFocus={i===0}
           type="number"
         />
-          <p className=" text-right text-gray-800 text-2xl">{score}</p>
+          <p className={`pr-2 text-right text-gray-800 text-xl ${scoreColor}`}>{score}</p>
             </div>
               {i !== 3 && <hr />}
             </div>
